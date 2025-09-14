a2fcd6382412160e05b1f529aa345b33d05c5d33
marcelodanieldm <danielmarcelo83@gmail.com>
2025-09-14 14:56:30 -0300

    add data seed in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,62 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Crear administrador
+admin = User.create!(
+  email: 'admin@clinica.com',
+  password: '12345678',
+  role: 'admin'
+)
+
+# Crear médico
+medico = User.create!(
+  email: 'dr.perez@clinica.com',
+  password: '12345678',
+  role: 'medico'
+)
+
+Paciente.create!(
+  user: medico,
+  nombre: 'Dr. Juan',
+  apellido: 'Pérez',
+  dni: '11223344',
+  telefono: '1199887766',
+  tipo_seguro: 'particular'
+)
+
+# Crear empleado
+empleado = User.create!(
+  email: 'empleado@clinica.com',
+  password: '12345678',
+  role: 'empleado'
+)
+
+# Crear pacientes
+10.times do |i|
+  Paciente.create!(
+    user: User.create!(
+      email: "paciente#{i+1}@email.com",
+      password: '12345678',
+      role: 'paciente'
+    ),
+    nombre: Faker::Name.first_name,
+    apellido: Faker::Name.last_name,
+    dni: Faker::Number.number(digits: 8),
+    telefono: Faker::PhoneNumber.phone_number,
+    tipo_seguro: %w[particular prepaga].sample
+  )
+end
+
+# Crear turnos
+medico = User.find_by(email: 'dr.perez@clinica.com')
+pacientes = Paciente.all
+
+(1..15).each do |i|
+  Turno.create!(
+    medico: medico,
+    paciente: pacientes.sample,
+    fecha: Date.today + i.days,
+    hora: "#{rand(8..18)}:#{rand([0,30])}",
+    estado: 'disponible'
+  )
+end
