ff53dbba36669053c2833bd8a11c1cb7b30dbe21
marcelodanieldm <danielmarcelo83@gmail.com>
2025-09-14 14:39:24 -0300

    added application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def role_class(user)
+    case user.role
+    when 'paciente' then 'badge bg-info'
+    when 'medico' then 'badge bg-success'
+    when 'empleado' then 'badge bg-warning'
+    when 'admin' then 'badge bg-danger'
+    end
+  end
 end
